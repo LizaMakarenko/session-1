1a89910ec254a82f51c5b704120872a9704537c6
Elizaveta Makarenko <lizzamakmakarenko@yandex.ru>
2025-06-20 18:20:12 +0300

    Second problem done

--- src/main/java/ru/sbt/jschool/session1/Problem2.java
+++ src/main/java/ru/sbt/jschool/session1/Problem2.java
@@ -1,12 +1,15 @@
 package ru.sbt.jschool.session1;
 
-import java.util.Map;
+
+import java.util.Properties;
 
 public class Problem2 {
     public static void main(String[] args) {
-        Map<String, String> env = System.getenv();
-        for (Map.Entry<String, String> entry : env.entrySet()) {
-            System.out.println(entry.getKey() + ":" + entry.getValue());
+        Properties properties = System.getProperties();
+        for (String key : properties.stringPropertyNames()) {
+            String value = properties.getProperty(key);
+            System.out.println(key + ": " + value);
         }
     }
 }
+
